1d0b10e81939f5f356213c84be81121964cb0fe6
AntonFly <anton99910@gmail.com>
2022-04-04 18:54:08 +0300

    First try lab2

---
+++ lab2/StandAlone/src/main/java/controllers/PersonWebService.java
@@ -0,0 +1,88 @@
+package controllers;
+
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.sql.DataSource;
+import javax.xml.bind.annotation.XmlElement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static controllers.ConnectionUtil.getConnection;
+
+@WebService(serviceName = "PersonService")
+public class PersonWebService {
+
+
+    @WebMethod(operationName = "getAllPersons")
+    public List<User> getPersons() {
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.getPersons();
+    }
+    @WebMethod(operationName = "getPersonsByName")
+    public List<User> getPersonsByName(@WebParam(name = "personName")
+                                                 String name) {
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.getPersonsByName(name);
+    }
+
+    @WebMethod(operationName = "getPersonsByNameAndSurname")
+    public List<User> getPersonsByNameAndSurname(@WebParam(name = "personName") String name,
+                                                 @WebParam(name = "personSurname") String surname) {
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.getPersonsByNameAndSurname(name, surname);
+    }
+
+    @WebMethod(operationName = "getPersonsByEmail")
+    public List<User> getPersonsByEmail(@WebParam(name = "personEmail")
+                                               String email) {
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.getPersonsByEmail(email);
+    }
+
+    @WebMethod(operationName = "getPersonsByPhone")
+    public List<User> getPersonsByPhone(@WebParam(name = "personPhone")
+                                               String phone) {
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.getPersonsByPhone(phone);
+    }
+
+    @WebMethod(operationName = "createPearson")
+    public int createUser(@WebParam(name = "name") @XmlElement(required = true) String name,
+                          @WebParam(name = "surname") @XmlElement(required = true) String surname,
+                          @WebParam(name = "phoneNumber") @XmlElement(required = true) int number,
+                          @WebParam(name = "email") @XmlElement(required = true) String email,
+                          @WebParam(name = "password") @XmlElement(required = true) String password){
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.createUser(name,surname,number,email,password);
+    }
+
+    @WebMethod(operationName = "deletePearson")
+    public String deleteUser(@WebParam(name = "id") @XmlElement(required = true) int id){
+
+        return  new UserDAO(getConnection()).deleteUser(id);
+    }
+
+    @WebMethod(operationName = "updatePearson")
+    public String updateUser(@WebParam(name = "name")  String name,
+                            @WebParam(name = "surname")  String surname,
+                            @WebParam(name = "phoneNumber")  int number,
+                            @WebParam(name = "email")  String email,
+                            @WebParam(name = "password")  String password,
+                            @WebParam(name = "id") @XmlElement(required = true) int id  ){
+        UserDAO dao = new UserDAO(getConnection());
+        return dao.updateUser(name,surname,number,email,password,id);
+    }
+
+
+
+
+
+
+
+
+}
---
+++ lab2/StandAlone/src/main/java/mypackage/User.java
@@ -0,0 +1,406 @@
+/**
+ * User.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package mypackage;
+
+public class User  implements java.io.Serializable {
+    private java.lang.String bio;
+
+    private java.util.Calendar birthday;
+
+    private java.lang.String email;
+
+    private int id;
+
+    private java.lang.String name;
+
+    private java.lang.String password;
+
+    private java.lang.String patronymic;
+
+    private int phone_number;
+
+    private java.lang.String surname;
+
+    public User() {
+    }
+
+    public User(
+           java.lang.String bio,
+           java.util.Calendar birthday,
+           java.lang.String email,
+           int id,
+           java.lang.String name,
+           java.lang.String password,
+           java.lang.String patronymic,
+           int phone_number,
+           java.lang.String surname) {
+           this.bio = bio;
+           this.birthday = birthday;
+           this.email = email;
+           this.id = id;
+           this.name = name;
+           this.password = password;
+           this.patronymic = patronymic;
+           this.phone_number = phone_number;
+           this.surname = surname;
+    }
+
+
+    /**
+     * Gets the bio value for this User.
+     * 
+     * @return bio
+     */
+    public java.lang.String getBio() {
+        return bio;
+    }
+
+
+    /**
+     * Sets the bio value for this User.
+     * 
+     * @param bio
+     */
+    public void setBio(java.lang.String bio) {
+        this.bio = bio;
+    }
+
+
+    /**
+     * Gets the birthday value for this User.
+     * 
+     * @return birthday
+     */
+    public java.util.Calendar getBirthday() {
+        return birthday;
+    }
+
+
+    /**
+     * Sets the birthday value for this User.
+     * 
+     * @param birthday
+     */
+    public void setBirthday(java.util.Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+
+    /**
+     * Gets the email value for this User.
+     * 
+     * @return email
+     */
+    public java.lang.String getEmail() {
+        return email;
+    }
+
+
+    /**
+     * Sets the email value for this User.
+     * 
+     * @param email
+     */
+    public void setEmail(java.lang.String email) {
+        this.email = email;
+    }
+
+
+    /**
+     * Gets the id value for this User.
+     * 
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+
+    /**
+     * Sets the id value for this User.
+     * 
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Gets the name value for this User.
+     * 
+     * @return name
+     */
+    public java.lang.String getName() {
+        return name;
+    }
+
+
+    /**
+     * Sets the name value for this User.
+     * 
+     * @param name
+     */
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * Gets the password value for this User.
+     * 
+     * @return password
+     */
+    public java.lang.String getPassword() {
+        return password;
+    }
+
+
+    /**
+     * Sets the password value for this User.
+     * 
+     * @param password
+     */
+    public void setPassword(java.lang.String password) {
+        this.password = password;
+    }
+
+
+    /**
+     * Gets the patronymic value for this User.
+     * 
+     * @return patronymic
+     */
+    public java.lang.String getPatronymic() {
+        return patronymic;
+    }
+
+
+    /**
+     * Sets the patronymic value for this User.
+     * 
+     * @param patronymic
+     */
+    public void setPatronymic(java.lang.String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+
+    /**
+     * Gets the phone_number value for this User.
+     * 
+     * @return phone_number
+     */
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+
+    /**
+     * Sets the phone_number value for this User.
+     * 
+     * @param phone_number
+     */
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+
+    /**
+     * Gets the surname value for this User.
+     * 
+     * @return surname
+     */
+    public java.lang.String getSurname() {
+        return surname;
+    }
+
+
+    /**
+     * Sets the surname value for this User.
+     * 
+     * @param surname
+     */
+    public void setSurname(java.lang.String surname) {
+        this.surname = surname;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof User)) return false;
+        User other = (User) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.bio==null && other.getBio()==null) || 
+             (this.bio!=null &&
+              this.bio.equals(other.getBio()))) &&
+            ((this.birthday==null && other.getBirthday()==null) || 
+             (this.birthday!=null &&
+              this.birthday.equals(other.getBirthday()))) &&
+            ((this.email==null && other.getEmail()==null) || 
+             (this.email!=null &&
+              this.email.equals(other.getEmail()))) &&
+            this.id == other.getId() &&
+            ((this.name==null && other.getName()==null) || 
+             (this.name!=null &&
+              this.name.equals(other.getName()))) &&
+            ((this.password==null && other.getPassword()==null) || 
+             (this.password!=null &&
+              this.password.equals(other.getPassword()))) &&
+            ((this.patronymic==null && other.getPatronymic()==null) || 
+             (this.patronymic!=null &&
+              this.patronymic.equals(other.getPatronymic()))) &&
+            this.phone_number == other.getPhone_number() &&
+            ((this.surname==null && other.getSurname()==null) || 
+             (this.surname!=null &&
+              this.surname.equals(other.getSurname())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getBio() != null) {
+            _hashCode += getBio().hashCode();
+        }
+        if (getBirthday() != null) {
+            _hashCode += getBirthday().hashCode();
+        }
+        if (getEmail() != null) {
+            _hashCode += getEmail().hashCode();
+        }
+        _hashCode += getId();
+        if (getName() != null) {
+            _hashCode += getName().hashCode();
+        }
+        if (getPassword() != null) {
+            _hashCode += getPassword().hashCode();
+        }
+        if (getPatronymic() != null) {
+            _hashCode += getPatronymic().hashCode();
+        }
+        _hashCode += getPhone_number();
+        if (getSurname() != null) {
+            _hashCode += getSurname().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(User.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://controllers/", "user"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("bio");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "bio"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("birthday");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "birthday"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "dateTime"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("email");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "email"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("id");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "id"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("name");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "name"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("password");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "password"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("patronymic");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "patronymic"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("phone_number");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "phone_number"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("surname");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "surname"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
